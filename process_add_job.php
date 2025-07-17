f5108d206ab67dce462d127e1c118e7a3c815032
devshanuuu <shanuuu0207@gmail.com>
2025-07-17 18:28:40 +0530

    Added job form submission code file

---
+++ process_add_job.php
@@ -0,0 +1,48 @@
+<?php
+
+// Connect to the database using the db.php
+ include 'db.php';
+
+// Check if form was submitted using POST
+
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $title = $_POST['job_title'];
+    $description = $_POST['job_description'];
+    $location = $_POST['location'];
+    $salary = $_POST['salary'];
+    $company = $_POST['company'];
+
+ 
+
+// Check if required fields are not empty
+
+if(!empty($title) && !empty($description) && !empty($location)) {
+
+    // SQL query to insert data
+    $stmt = $conn->prepare("INSERT INTO jobs (job_title, job_description, location, salary, company) VALUES (?, ?, ?, ?, ?)");
+
+  
+    // Bind the actual form values to the SQL placeholders
+    // "sssss" means all 5 values are strings
+    $stmt->bind_param("sssss", $title, $description, $location, $salary, $company);
+
+    // Try executing the query
+    if($stmt->execute()) {
+        
+        //If success redirect to add_job.php with a success flag in url
+        header("Location: add_job.php?success=1");
+        exit;
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+   } else {
+    
+    // If required fields are missing
+    echo "please fill all required fields."; 
+   }
+}
+
+?>
